40f82be86a43337589ed197def470bcc7cdfd22a
HMZHZDH7 <56533642+HMZHZDH7@users.noreply.github.com>
2019-11-06 09:08:05 +0100

    Update Main.py

--- P3_Proto/Main.py
+++ P3_Proto/Main.py
@@ -2,7 +2,6 @@ from Client import*
 from image_Processor import*
 
 dividend = 3
-
 IP = imageProcessor()  # ......................................................Instantiate imageProcessor object.
 IP.create_blob_detector()
 Client = client()  # ..........................................................Instantiate client object.
@@ -36,11 +35,13 @@ while True:  # ................................................................I
         Client.SockConnect()
         Client.clientConnected = True
 
-    if (Client.clientConnected is True) and (IP.distance_init != 0.0) and \
-            (IP.pos_right_hand[1] >= IP.guitar_string_pos - 10) and \
-            (IP.pos_right_hand[1] <= IP.guitar_string_pos + 10):
+    if (IP.pos_right_hand[1] <= stringPos - 10) or (IP.pos_right_hand[1] >= stringPos + 10):
+        Client.spamFilter = True
+
+    if Client.clientConnected and (IP.distance_init != 0.0) and (IP.pos_right_hand[1] >= stringPos - 10) and (IP.pos_right_hand[1] <= stringPos + 10) and Client.spamFilter:
         IP.distance = IP.distance / dividend
         Client.SendInfo(0, int(IP.distance), int(IP.speed_right_hand))
+        Client.spamFilter = False
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
