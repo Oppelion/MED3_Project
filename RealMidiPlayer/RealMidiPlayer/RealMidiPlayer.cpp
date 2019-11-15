9a5b012ba2bf4db4d10f10b4d5de9771479fe789
Oppelion <56341984+Oppelion@users.noreply.github.com>
2019-11-15 12:10:02 +0100

    Comment

--- RealMidiPlayer/RealMidiPlayer/RealMidiPlayer.cpp
+++ RealMidiPlayer/RealMidiPlayer/RealMidiPlayer.cpp
@@ -8,78 +8,78 @@
 
 using namespace std;
 
-bool getData = false;
-const int maxSection = 13;			//The number of sectors that the program can choose from, representing the neck intervals of one string(1-2 and 2-3)
-int dividend = 4;
-int counterStart = 2;
-int recvArray[3]; //place 0 is the max distance, place 1 is current distance, and the velocity
-int playedSounds[2][20];
-int sector;
-int notes[13] = { 0, 96, 90, 84, 78, 72, 66, 60, 54, 50, 48, 46, 44 };
-float sections[maxSection];         //This array stores the size of the different sectors for the sorting algorithm
+const int maxSection = 13;												                             //The number of sectors that the program can choose from, representing the neck intervals of one string(1-2 and 2-3)
 
-void section_Calculator() {
-	unsigned int maxDistance;
-	maxDistance = (float)recvArray[0] * 4 * 1.2f;
-	float sectionSize = maxDistance / (maxSection - 1);    //Calculates the size that each section has to have as a function of the different sectors and the distance inputted.
-	for (int i = 0; i <= maxSection - 1; i++) {	//Calculates and assigns the starting point of each section.
-		sections[i] = sectionSize * i;			// Assignes the value based on its postions on the neck(A to B)
+int dividend = 4;														                             //Uses to multiply data recived, because the recived data is divided in the Python part of the program (Called dividend because of naming in Python part)
+int counterStart = 2;                                                                                //This integer increments up as you play tunes.
+int recvArray[3];                                                                                    //Place 0 is the max distance, place 1 is current distance, and the velocity (used to store recived data)
+int playedSounds[2][20];												                             //Array used to store the note played and when it is started
+int notes[13] = { 0, 96, 90, 84, 78, 72, 66, 60, 54, 50, 48, 46, 44 };                               //Lowest 0 highest 127 (Notes that can be played based on chosen sector)
+
+float sections[maxSection];                                                                          //This array stores the size of the different sectors for the sorting algorithm
+
+bool getData = false;													                             //Race condition for when to play sounds
+
+void section_Calculator()												                             //section_Calculator is run once when the program has received the first set of data from the server. It calculates each of the sections, where a different note will be played.
+{                                             
+	unsigned int maxDistance;											                             //The maximum allowed distance between BLOBs (Distance can't be below zero)
+	maxDistance = (float)recvArray[0] * 4 * 1.2f;                                                    //maxDistance is calculated by first multiplying the received distance by 4, to return it to its previous state, and then multiplied again by 1.2, to give the player the maximum distance their hands has to be within. 
+	float sectionSize = maxDistance / (maxSection - 1);					                             //Calculates the size that each section has to have as a function of the different sectors and the distance inputted.
+
+	for (int i = 0; i <= maxSection - 1; i++)							                             //Calculates and assigns the starting point of each section.
+	{	
+		sections[i] = sectionSize * i;									                             //Assignes the value based on its postions on the neck(A to B)
 	}
 }
 
-int noteAssigner()
-{                           //This functions runs through the different sectors assigned above and finds out which one the inputted length matches to.
-	recvArray[1] = recvArray[1] * 4;
-	cout << "Semi-cooked data after treatment: " << recvArray[1] << "\n";
+int noteAssigner()                                                                                   //noteAssigner takes the received current distance and finds out which section the values is within, to choose which tone has to be played.
+{
+	recvArray[1] = recvArray[1] * 4;                                                                 //Returns the received value into its original value.
+	for (int i = 0; i < maxSection; i++)                                                             //This for loop uses maxsection to find out how manny times it has to run.
+	{
+		if (recvArray[1] >= sections[i] && recvArray[1] < sections[i + 1])                           //If the received value is within these two points, you have found the sector it belongs to.
+		{
+			int sector = i + 1;											                             //The sector to be played (Sector not allow to be zero, hence the +1)
 
-	for (int i = 0; i < maxSection; i++) {
-		if (recvArray[1] >= sections[i] && recvArray[1] < sections[i + 1]) {
-			int sector = i + 1;
-			cout << "This is the sector that the thing registers as: " << sector << "\n";
-
-			return sector;
+			return sector;                                                                           //Returns the value to main.
 		}
 	}
-	// cout << "The length inputted is either above or below the maximum arm distance. \n";
-	return 1;
+	cout << "The length inputted is either above the maximum arm distance or below zero." << endl;   //Cout statement to point out if a flaw with the maximum distance is detected
+	return 1;																						 //Default return
 }
 
-float volumeAssigner()
+float volumeAssigner()                                                                               //This function calculates the volume that the note has to be played at.
 {
-	float distFloat = (float)recvArray[2] / 100;
-	// cout << "Float is:" << distFloat << "\n";
-	return distFloat;
+	float distFloat = (float)recvArray[2] / 100;													//Converts the float to a percentage
+	return distFloat;                                                                               //Returns the value to main.
 }
 
 int main()
 {
-	start_Sound();
-	thread ServerThread(server_main);
-	ServerThread.detach();
-	cout << "New Thread" << endl;
+	start_Sound();                                                                                  //start_Sound sets up the necessary settings for the soundfont to work, then plays one note to load the soundfont into memory.
+	thread ServerThread(server_main);			                                                    //Thread for the server, so that the main and server can run independtly of each other.
+	ServerThread.detach();						                                                    //Detaches the server thread, since the main and server does not need to directly communicate.
+	cout << "New Thread" << endl;																	//Print statement to verify thread is succesfully created.
 
-	while (true) {
-		if (getData == true) {
-			if (recvArray[0] != 0)
+	while (true) {                                                                                  //This while loop runs infinitely until the program is shut down.
+		if (getData == true)                                                                        //This if statement ensures that the program will not play several notes in quick succession if the user did not do it on purpose. 
+		{
+			if (recvArray[0] != 0)                                                                  //This function is only run once when the program has gotten its first set of data.
 			{
-				section_Calculator();
+				section_Calculator();																//Function found on line 23
 			}
 
-			playedSounds[1][counterStart] = clock();
-			playedSounds[0][counterStart] = notes[noteAssigner()];
-			cout << "This is the value registered in the array: " << playedSounds[0][counterStart] << "\n";
+			playedSounds[1][counterStart] = clock();												//Sets the time at which is sound is being played using counterStart to choose the placement inside the 2D-array.
+			playedSounds[0][counterStart] = notes[noteAssigner()];									//Sets the note being played by the same method as above.
 
+			playNote();                                                                             //Function found in Sound.cpp line 32.
+			counterStart++;																			//Increment counterStarter by 1 :-)
 
-			playNote();
-			counterStart++;
-
-			if (counterStart >= 19) {
-				counterStart = 2;
+			if (counterStart >= 19) {                                                               //Once the counterStart reaches 19, it is reset.
+				counterStart = 2;																	//Resets counterStart to 2, since the array bugs if this equals 1.
 			}
-			//SDL_Delay(500);
-			getData = false;
+			getData = false;																		//Cancels the while loop until next time data is received through server.
 		};
-		
-		stopNote();
+		stopNote();                                                                                 //Function found in Sound.cpp line 38
 	}
 }
--- RealMidiPlayer/RealMidiPlayer/Sound.cpp
+++ RealMidiPlayer/RealMidiPlayer/Sound.cpp
@@ -6,16 +6,16 @@
 
 using namespace std;
 
-queue <int> soundQueue;
-bool soundLoad = true;
-int counterEnd = 2;
+queue <int> soundQueue;                                                                                    //The queue is implemented to make a first-in-first-out system when shutting off tones.
+bool soundLoad = true;                                                                                     //soundLoad toggles the note that is played when the program is initially started.
+int counterEnd = 2;                                                                                        //Serves the same purpose as counterStart in RealMidiPLayer.cpp
 
 // Holds the global instance pointer(copy)
 
-static tsf* soundFont;
+static tsf* soundFont;                                                                                     //Creates an object that will be referred to through the entire code.
 
 
-static void AudioCallback(void* data, Uint8* stream, int len)
+static void AudioCallback(void* data, Uint8* stream, int len)                                              //This function is responsible for taking ths setup data and applying it to the note we are playing.
 {
 	int SampleCount = (len / (2 * sizeof(float))); //2 output channels
 	tsf_render_float(soundFont, (float*)stream, SampleCount, 0);
@@ -29,21 +29,23 @@ Start playing a note
  vel: velocity as a float between 0.0 (equal to note off) and 1.0 (full)
  */
 
-void playNote()
+void playNote()																	                           //Function starts the sound to be played and pushes to the queue.
 {
-	//These three lines below play the three notes that make up the cord for each section
-	tsf_note_on(soundFont, 29, playedSounds[0][counterStart], volumeAssigner());
-	soundQueue.push(playedSounds[1][counterStart] + 1000);
+	tsf_note_on(soundFont, 29, playedSounds[0][counterStart], volumeAssigner());                           //Starts sound using the soundfont, index of the instrument, the note, and the volume.
+	soundQueue.push(playedSounds[1][counterStart] + 1000);						                           //Puts the timer into the queue, so it end 1000 milliseconds after the note was initially played.
 }
 
-void stopNote() {
-	if (soundQueue.empty() == false) {
-		if (clock() >= soundQueue.front()) {
-			tsf_note_off(soundFont, 29, playedSounds[0][counterEnd]);
-			cout << "CounterEnd: " << counterEnd << "\n";
-			soundQueue.pop();
+void stopNote()																	                           //Function that stops sound which is currently being played.
+{
+	if (soundQueue.empty() == false)                                                                       //If the queue is read when there is no data in it, the program will crash. This will prohibit the code from running this part if the queue is empty.
+	{
+		if (clock() >= soundQueue.front())                                                                 //If the current clock is equal to the time registered in the foremost value, run the rest of this function.
+		{
+			tsf_note_off(soundFont, 29, playedSounds[0][counterEnd]);                                      //This stops the note from playing, by using the counterEnd to find the note that has to be closed.
+			soundQueue.pop();                                                                              //pop destroys the foremost value in the queue, automatically moving the value behind to the front.
 			counterEnd++;
-			if (counterEnd >= 19) {
+			if (counterEnd >= 19)                                                                          //When counterEnd reaches 19, reset it.
+			{
 				counterEnd = 2;
 			}
 		}
@@ -51,23 +53,24 @@ void stopNote() {
 }
 
 
-int start_Sound() {
-	// Define the desired audio output format we request
+int start_Sound() 
+{
+	//Define the desired audio output format we request
 	SDL_AudioSpec OutputAudioSpec;
-	OutputAudioSpec.freq = 44100;
-	OutputAudioSpec.format = AUDIO_F32;
-	OutputAudioSpec.channels = 2;
+	OutputAudioSpec.freq = 44100;                                                                          //Sets the frequency of which the audio needs to be played on.
+	OutputAudioSpec.format = AUDIO_F32;                                                                    //Sets the audio format.
+	OutputAudioSpec.channels = 2;                                                                          //Sets the channel the audio will be played on.
 	OutputAudioSpec.samples = 4096;
 	OutputAudioSpec.callback = AudioCallback;
 
-	// Initialize the audio system
+	//Initialize the audio system
 	if (SDL_AudioInit(TSF_NULL) < 0)
 	{
 		fprintf(stderr, "Could not initialize audio hardware or driver\n");
 		return 1;
 	}
 
-	// Load the SoundFont from a file
+	//Load the SoundFont from a file
 	soundFont = tsf_load_filename("Soundfont1.sf2");
 	if (!soundFont)
 	{
@@ -75,26 +78,27 @@ int start_Sound() {
 		return 1;
 	}
 
-	// Set the SoundFont rendering output mode
+	//Set the SoundFont rendering output mode
 	tsf_set_output(soundFont, TSF_STEREO_INTERLEAVED, OutputAudioSpec.freq, 0);
 
-	// Request the desired audio output format
+	//Request the desired audio output format
 	if (SDL_OpenAudio(&OutputAudioSpec, TSF_NULL) < 0)
 	{
 		fprintf(stderr, "Could not open the audio hardware or the desired audio output format\n");
 		return 1;
 	}
 
-	// Start the actual audio playback here
-	// The audio thread will begin to call our AudioCallback function
+	//Start the actual audio playback here
+	//The audio thread will begin to call our AudioCallback function
 	//Interval for guitar 29 starts at 23 to 126
 	//Interval for guitar 30 starts at 0 to 126
 
 
 	SDL_PauseAudio(0);
-	if (soundLoad == true) {
+	if (soundLoad == true) 
+	{
 		tsf_note_on(soundFont, 30, 60, 0.0f);
-		std::cout << "Sound is loaded \n";
+		cout << "Sound is loaded" << endl;
 		soundLoad = false;
 	}
 	return 0;
